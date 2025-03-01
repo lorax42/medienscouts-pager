935b92db96abb466c834040286cb9a4156c7f412
lorax <142108266+lorax42@users.noreply.github.com>
2025-03-01 09:05:52 +0100

    feat(server): Add removal of old messages
    - daemon now automatically removes old messages after grace period

--- server/daemon.py
+++ server/daemon.py
@@ -14,6 +14,9 @@ import json
 DOMAIN = ""
 EMAIL_ADRESS = ""
 PASSWD = ""
+MESSAGE_GRACE_TIME = ""
+
+TZ_INFO = None
 
 
 def get_new_messages() -> [Dict[str, str | datetime.datetime]]:
@@ -26,13 +29,8 @@ def get_new_messages() -> [Dict[str, str | datetime.datetime]]:
     with MailBox(DOMAIN).login(EMAIL_ADRESS, PASSWD) as mailbox:
         # Fetch emails
         for mssg in mailbox.fetch(AND(seen=False)):
-            subject = mssg.subject
-            print(mssg.uid)
-            print(mssg.date)
-            print(subject)
-
             messages.append({
-                "id": mssg.uid,
+        "id": mssg.uid,
                 "timestamp": mssg.date,
                 "message": mssg.subject,
             })
@@ -40,12 +38,45 @@ def get_new_messages() -> [Dict[str, str | datetime.datetime]]:
     return messages
 
 
-def remove_old(messages, grace_period: int):
+def remove_old(messages, grace_period: int) -> [Dict[str, str | datetime.datetime]]:
     """
     remove old messages based on given grace period (in s)
     """
+    timely_messages = []
+
+    now = datetime.datetime.now(TZ_INFO)
+
+    # iterate over messages
     for mssg in messages:
-        ...
+        # get time diff between now and message time
+        messagetime = mssg['timestamp']
+
+        if not isinstance(messagetime, datetime.datetime):
+            messagetime = datetime.datetime.fromisoformat(messagetime)
+            mssg['timestamp'] = messagetime
+        
+        print(messagetime)
+        deltatime = now - messagetime
+        deltatime = deltatime.seconds
+
+        # only pass on messages that have a lower dt than the grace_period
+        if deltatime < grace_period:
+            timely_messages.append(mssg)
+        else:
+            print("I: removing message %s" % mssg['id'])
+
+    return timely_messages
+
+
+def write_data(messages):
+    """
+    write message data to JSON file
+    """
+    for mssg in messages:
+        mssg['timestamp'] = mssg['timestamp'].isoformat()
+
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(messages, f)
 
 
 def main():
@@ -59,11 +90,19 @@ def main():
     global DOMAIN
     global EMAIL_ADRESS
     global PASSWD
+    global MESSAGE_GRACE_TIME
 
-    # set env vars
     DOMAIN = os.getenv('DOMAIN')
     EMAIL_ADRESS = os.getenv('EMAIL_ADRESS')
     PASSWD = os.getenv('PASSWD')
+    MESSAGE_GRACE_TIME = int(os.getenv('MESSAGE_GRACE_TIME'))
+
+    print("MESSAGE_GRACE_TIME: %d" % MESSAGE_GRACE_TIME)
+
+    # get timezone info
+    global TZ_INFO
+    now = datetime.datetime.now()  # current datetime
+    TZ_INFO = now.astimezone().tzinfo
 
     messages = []
 
@@ -71,25 +110,14 @@ def main():
 
     # event loop
     while True:
-        # messages += get_new_messages()  # append new messages
-        # remove_old(messages, 60*5)  # remove old messages
-        messages = [
-            {
-                "id": "123",
-                "timestamp": "2025-02-28...",
-                "message": "Hello",
-            },
-            {
-                "id": "124",
-                "timestamp": "2025-02-28...",
-                "message": "Hello again",
-            },
-        ]
+        messages += get_new_messages()  # append new messages
+        messages = remove_old(messages, MESSAGE_GRACE_TIME)  # remove old messages
 
-        with open("data.json", "w", encoding="utf-8") as f:
-            json.dump(messages, f)
+        print(messages)
 
-        time.sleep(10)
+        write_data(messages)  # write data to JSON
+
+        time.sleep(5)
 
 
 if __name__ == "__main__":
